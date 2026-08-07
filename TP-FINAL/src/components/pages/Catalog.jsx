e7db3a5bd0f83c85a96096090bfec7b5e49fb20a
colo <coloporta2302@gmail.com>
2026-08-07 17:31:25 -0300

    Conectar cada producto con el carrito

--- TP-FINAL/src/components/pages/Catalog.jsx
+++ TP-FINAL/src/components/pages/Catalog.jsx
@@ -4,13 +4,15 @@ import { ProductCard } from "../molecules";
 import { products } from "../../data/products";
 
 export default function Catalog() {
-  const [Catproducts, setCatproducts] = useState(products);
+  const [Catproducts] = useState(products);
 
   return (
     <section>
       {Catproducts.length ? (
         Catproducts.map((product) => (
           <ProductCard
+            key={product.id}
+            id={product.id}
             title={product.title}
             imgSource={product.imgSource}
             description={product.description}
