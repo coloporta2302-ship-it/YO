0f061de8825c0e83414717dec761c8460a36f746
colo <coloporta2302@gmail.com>
2026-08-07 17:31:19 -0300

    Agregar botón para añadir productos al carrito

--- TP-FINAL/src/components/molecules/ProductCard.jsx
+++ TP-FINAL/src/components/molecules/ProductCard.jsx
@@ -1,17 +1,35 @@
 import { Heading, Paragraph } from "../atoms";
+import { useCart } from "../context/CartContext";
+
 export default function ProductCard({
+  id,
   title,
   description,
   imgSource,
   price,
   className = "",
 }) {
+  const { addToCart } = useCart();
+
+  const handleAddToCart = () => {
+    addToCart({
+      id,
+      nombre: title,
+      precio: Number(String(price).replace(/\./g, "").replace(/,/g, ".")),
+      imagen: imgSource,
+    });
+  };
+
   return (
     <article className={className}>
       <Heading size="h3" text={title} />
       <img src={imgSource} alt={title} />
       <Paragraph text={description} />
       <Heading size="h5" text={price} />
+
+      <button type="button" onClick={handleAddToCart}>
+        🛒 Agregar al carrito
+      </button>
     </article>
   );
 }
